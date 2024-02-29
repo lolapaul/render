b20ad0c9a68bdd8a042141b38d313687c3d9baa6
fatamorgana <dvillanueva@up.edu.mx>
2024-02-28 21:00:33 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.header('Lanzar una moneda')
+st.write('Esta aplicación aún no es funcional. En construcción.[F[[C[C[D[D[C[Ds[C[D[C[D[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C')
