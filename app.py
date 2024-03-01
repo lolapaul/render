e6b1a7a7fbf2d49e4f00c1b321a506ae03333e45
fatamorgana <dvillanueva@up.edu.mx>
2024-03-01 07:22:13 -0600

    initial commit

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ number_of_trials = st.slider('¿Número de intentos?',1,1000,10)
 start_button = st.button('Ejecutar')
 
 if start_button:
-    st.write(f'Experimento con {number_of_trials} intentos en curso.')
-
-st.write('Esta aplicación aún no es funcional. En construcción.')
+    st.write(f'Experimento con {number_of_trials} inten    tos en curso.')
+    
+    st.write('Esta aplicación aún no es funcional. En c    onstrucción.')
